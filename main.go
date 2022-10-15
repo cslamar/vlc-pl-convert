986314f74dbe01882b669fc2d74b6b3d416220cf
Chris Slamar <chris.slamar@hologram.io>
2022-10-15 09:52:19 -0500

    framework

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+type Playlist struct {
+	XMLName   xml.Name `xml:"playlist"`
+	Ns        string   `xml:"xmlns,attr"`
+	VlcNs     string   `xml:"xmlns:vlc,attr"`
+	Version   int      `xml:"version,attr"`
+	Title     string   `xml:"title"`
+	TrackList []Track  `xml:"trackList>track"`
+	Extension Ext      `xml:"extension"`
+}
+
+type Track struct {
+	XMLName         xml.Name `xml:"track"`
+	Location        string   `xml:"location"`
+	Duration        int      `xml:"duration"`
+	Title           string   `xml:"title,omitempty"`
+	Creator         string   `xml:"creator,omitempty"`
+	TrackExtensions VlcId    `xml:"extension"`
+}
+
+type VlcId struct {
+	Application string `xml:"application,attr"`
+	Id          int    `xml:"vlc:id,omitempty"`
+}
+
+type VlcItem struct {
+	XMLName xml.Name `xml:"vlc:item"`
+	Tid     int      `xml:"tid,attr"`
+}
+
+type Ext struct {
+	XMLName     xml.Name  `xml:"extension"`
+	Application string    `xml:"application,attr"`
+	Tids        []VlcItem `xml:"tids"`
+}
+
+func main() {
+
+	//testItOut()
+	parseFile("test.xspf")
+}
+
+func parseFile(filename string) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var playlist = Playlist{}
+
+	if err := xml.Unmarshal(data, &playlist); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(playlist)
+}
+
+//func testItOut() {
+//	t := Track{
+//		Location: "vlc://pause:2",
+//		Duration: 2,
+//		TrackExtensions: VlcId{
+//			Application: "http://www.videolan.org/vlc/playlist/0",
+//			Id:          1,
+//		},
+//	}
+//
+//	item := VlcItem{
+//		Tid: 1,
+//	}
+//
+//	e := Ext{
+//		Application: "http://www.videolan.org/vlc/playlist/0",
+//		Tids:        []VlcItem{item, item},
+//	}
+//
+//	p := Playlist{
+//		Ns:        "http://xspf.org/ns/0/",
+//		VlcNs:     "http://www.videolan.org/vlc/playlist/ns/0/",
+//		Version:   1,
+//		Title:     "Something",
+//		TrackList: []Track{t, t},
+//		Extension: e,
+//	}
+//
+//	xmlPrint(p)
+//}
+
+func xmlPrint(input any) {
+	output, err := xml.MarshalIndent(input, "", "    ")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	finalOut := []byte(xml.Header + string(output))
+
+	fmt.Println(string(finalOut))
+}
